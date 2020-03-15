e0e1d3a3df6d07be30fd86f5532e3403e92038f9
denis <denis.denisenko@gmai.com>
2020-03-15 11:23:38 +0200

    New

---
+++ src/Class1.java
@@ -0,0 +1,25 @@
+
+public class Class1 {	
+        public double Calculate(double amount, int type, int years)
+        {
+            double priceAfterDiscount = 0;
+            double disc = (years > 5) ? (double)5 / 100 : (double)years / 100;
+            if (type == 1)
+            {
+                priceAfterDiscount = amount;
+            }
+            else if (type == 2)
+            {
+                priceAfterDiscount = (amount - (0.1f * amount)) - disc * (amount - (0.1f * amount));
+            }
+            else if (type == 3)
+            {
+                priceAfterDiscount = (0.7f * amount) - disc * (0.7f * amount);
+            }
+            else if (type == 4)
+            {
+                priceAfterDiscount = (amount - (0.5f * amount)) - disc * (amount - (0.5f * amount));
+            }
+            return priceAfterDiscount;
+        }
+}
---
+++ src/Program.java
@@ -0,0 +1,13 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		try {
+			double priceAfterDiscount = 0f;
+			System.out.println("Price after discout: " + priceAfterDiscount);
+		} catch (Exception e) {
+			System.out.println("Undefined Customer "+ e);
+		}
+	}
+
+}
